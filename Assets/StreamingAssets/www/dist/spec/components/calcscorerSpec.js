938a192f5da046db6c88917dc44315c8648a110e
moritomi <yutaka.moritomi@gmail.com>
2020-04-24 02:37:38 +0900

    042306＞StreamingAssets、Effectなど削除

--- Assets/StreamingAssets/www/dist/spec/components/calcscorerSpec.js
+++
@@ -1,9 +0,0 @@
-import {CalcScorer} from '../../components/calcscorer.js'
-
-describe("get operator", function() {
-
-  it("正常", function() {
-    let res=CalcScorer.operator();
-    // expect(res).toBeNaN();
-  });
-});
